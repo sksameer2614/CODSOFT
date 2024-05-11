6f70924a80b90efd5f64745c5560edbca9b741bf
sksameer2614 <150272175+sksameer2614@users.noreply.github.com>
2024-05-11 11:28:03 +0530

    Delete list2.py

--- list2.py
+++
@@ -1,9 +0,0 @@
-dict1 = dict()
-n = int(input("Enter the number of items: "))
-
-for i in range(n):
-    k = input("Enter the key: ")
-    v = input("Enter the value: ")
-    dict1[k] = v
-
-print(dict1)
