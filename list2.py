690bdc81aba7b1638241fea07e295f1862fc00cd
sksameer2614 <150272175+sksameer2614@users.noreply.github.com>
2024-05-11 11:17:38 +0530

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,9 @@
+dict1 = dict()
+n = int(input("Enter the number of items: "))
+
+for i in range(n):
+    k = input("Enter the key: ")
+    v = input("Enter the value: ")
+    dict1[k] = v
+
+print(dict1)
